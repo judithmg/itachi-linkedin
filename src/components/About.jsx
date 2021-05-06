38392e52acae014c837595061a88cb65f45068cc
judithmg <jmartigo33@gmail.com>
2021-05-06 20:16:02 +0200

    about comp with fake data

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section className="about__container">
+    <div className="about__quote">Lorem ipsum dolor sit amet, consectetur?</div>
+    <div className="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi beatae cumque laudantium sequi ducimus tempora consequatur reprehenderit quibusdam distinctio, quasi harum blanditiis? Quasi odio dolor molestiae consectetur perferendis voluptates aliquam!</div>
+  </section>
+);
+
+export default About;
