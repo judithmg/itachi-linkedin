0ab86c9b64392b75d55586ea8bd51d13f84d5e92
judithmg <jmartigo33@gmail.com>
2021-05-06 20:21:15 +0200

    SideNav with simple list

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SideBar = () => (
+  <section className="sidebar__container">
+    <h2>Itachi Uchiha</h2>
+    <h3>Nukenin at Akatsuki</h3>
+    <ul>
+      <li>Home</li>
+      <li>My Network</li>
+      <li>Jobs</li>
+      <li>Messaging</li>
+      <li>Notifications</li>
+    </ul>
+  </section>
+);
+
+export default SideBar;
