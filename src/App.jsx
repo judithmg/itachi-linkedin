be0c5cf4421e5cea7249e80c472c68d8ab67b663
judithmg <jmartigo33@gmail.com>
2021-05-08 12:59:02 +0200

    Main app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import React from 'react';
+import Pending from './components/Pending';
+import SideNav from './components/SideNav';
+import Connections from './components/Connections';
+import About from './components/About';
+
+import './styles/App.scss';
+import './styles/Prueba.scss';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <div className="left-column">
+        <SideNav />
+      </div>
+      <div className="middle-column">
+        <div className="middle-left" />
+        <div className="middle-middle">
+          <Connections />
+        </div>
+        <div className="middle-right">
+          <Pending />
+        </div>
+        <About />
+      </div>
+      <div className="right-column">
+        <div><h2>Jobs for you</h2></div>
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
