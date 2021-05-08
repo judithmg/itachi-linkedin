865509501620127966b03a11dcbcb9264b89ecaf
judithmg <jmartigo33@gmail.com>
2021-05-08 12:59:14 +0200

    Connections component

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Connection = ({ connection }) => (
   <div className="connection__container">
-    <img src={`${connection.name}.png`} alt="connection" />
+    <img src={`./images/${connection.name.split(/(\s+)/)[0].toLowerCase()}.png`} className="connection__img" alt="connection" />
     <div>
-      <p className="connection__name">{connection.name}</p>
+      <h3 className="connection__name">{connection.name}</h3>
       <p className="connection__position">
-        {`${connection.position} at ${connection.place}`}
+        {`${connection.position}
+         at
+         `}
+        <span>{connection.where}</span>
       </p>
     </div>
   </div>
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Connection from './Connection';
 import connections from '../data/connections';
+import '../styles/Connections.scss';
 
 const Connections = () => (
   <section className="connections__container">
     <div className="connections__header">
       <h2>Connections</h2>
-      <button type="button">View All</button>
+      <button className="btn__view-all" type="button">View All</button>
     </div>
     {connections.map((connection) => <Connection connection={connection} />)}
   </section>
