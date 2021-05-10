a77b21eba83f844b1b6b5737cf573c63643c3829
judithmg <jmartigo33@gmail.com>
2021-05-10 11:45:45 +0200

    modified background of sidebar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -3,8 +3,12 @@ import '../styles/SideNav.scss';
 
 const SideNav = () => (
   <section className="sidenav__container">
+    <img src="./images/itachi-icon.jpg" alt="itachi ico" />
     <h2>Itachi Uchiha</h2>
-    <h3>Nukenin at Akatsuki</h3>
+    <h3>
+      <span>Nukenin </span>
+      at Akatsuki
+    </h3>
     <ul>
       <li>Home</li>
       <li>My Network</li>
