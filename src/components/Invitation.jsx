2e31e527274aae1cc4cc079894b32b4ecc3a3d02
judithmg <jmartigo33@gmail.com>
2021-05-10 16:57:26 +0200

    added buttons

--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 
 const Invitation = ({ invitation }) => (
   <div className="invitation__container">
-    <img src={`./images/${invitation.name.split(/(\s+)/)[0].toLowerCase()}.png`} className="invitation__img" alt="connection" />
-    <div>
-      <p className="invitation__name">{invitation.name}</p>
-      <p className="invitation__position">
-        <span>
-          {invitation.position}
-          {' '}
-          at
-          {' '}
-        </span>
-        {invitation.where}
-
-      </p>
+    <div className="invitation__left">
+      <img src={`./images/${invitation.name.split(/(\s+)/)[0].toLowerCase()}.png`} className="invitation__img" alt="connection" />
+      <div>
+        <p className="invitation__name">{invitation.name}</p>
+        <p className="invitation__position">
+          <span>
+            {invitation.position}
+            {' '}
+            at
+            {' '}
+          </span>
+          {invitation.where}
+        </p>
+      </div>
+    </div>
+    <div className="invitation__buttons">
+      <button type="button">&#xf00c;</button>
+      <button type="button">&#xf00d;</button>
     </div>
   </div>
 );
