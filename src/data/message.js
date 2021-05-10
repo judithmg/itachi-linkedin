ed4400275a9628f3a72f706f7a81fcc6b82e1940
judithmg <jmartigo33@gmail.com>
2021-05-10 19:32:10 +0200

    Inbox messages data

---
+++ src/data/message.js
@@ -0,0 +1,20 @@
+export default [{
+  date: 'Today',
+  name: 'Orochimaru',
+  message: 'I want your body 🐍',
+},
+{
+  date: 'Yesterday',
+  name: 'Uzumaki Naruto',
+  message: 'A raven is coming out of me...',
+},
+{
+  date: '1 month ago',
+  name: 'Pain',
+  message: 'Pain',
+},
+{
+  date: '1 month ago',
+  name: 'Deidara',
+  message: 'I will still defeat you...',
+}];
