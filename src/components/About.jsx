1b84d2cf321bfe247d5386ea173407f88589be0c
judithmg <jmartigo33@gmail.com>
2021-05-10 19:31:57 +0200

    Added actual about info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 const About = () => (
   <section className="about__container">
-    <div className="about__quote">Lorem ipsum dolor sit amet, consectetur?</div>
-    <div className="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi beatae cumque laudantium sequi ducimus tempora consequatur reprehenderit quibusdam distinctio, quasi harum blanditiis? Quasi odio dolor molestiae consectetur perferendis voluptates aliquam!</div>
+    <div className="about__quote">It is foolish to fear what we have yet to see and know.</div>
+    <div className="about__text">
+      <div>
+        Itachi Uchiha (うちはイタチ, Uchiha Itachi) was a prodigy of Konohagakure&apos;s Uchiha clan and also served as an Anbu Captain. He later became an international criminal after murdering his entire clan, sparing only his younger brother, Sasuke. He afterwards joined the international criminal organisation known as Akatsuki, whose activity brought him into frequent conflict with Konoha and its ninja — including Sasuke — who sought to avenge their clan. Following his death, Itachi&apos;s motives were revealed to be more complicated than they seemed and that his actions were only ever in the interest of his brother and village, remaining a loyal shinobi of Konohagakure to the very end.
+      </div>
+
+      <div>Itachi was the first child born to Mikoto and Fugaku Uchiha. His early childhood was marked with violence: when he was four years old, the Third Shinobi World War waged and he witnessed first-hand many of the war&apos;s casualties. The death and destruction he experienced at such a young age traumatised Itachi and made him a pacifist. Furthermore, meeting Orochimaru, who viewed life without eternity as meaningless, also made Itachi greatly consider how to carry out his life. He became somewhat anti-social, focusing more on his training than enjoying his childhood. At the same time, he developed a kinship with crows. At age 5, Itachi met Shisui. With their similar views, the two quickly became best friends and formed a brother-like bond, training together and teaching each other new skills. During the Nine-Tailed Demon Fox&apos;s Attack, he was responsible for looking after his infant brother, Sasuke, and with his parents occupied, evacuated with his brother and Izumi Uchiha, to the south-east shelter. The Nine-Tails&apos; attack had soured relations between Konoha&apos;s leadership and the Uchiha, the former believing the latter to be responsible. The Uchiha were relocated to a corner of the village, isolating them from the rest of the village and making it easier to monitor them.</div>
+    </div>
   </section>
 );
 
