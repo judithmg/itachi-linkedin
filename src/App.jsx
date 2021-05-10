c57878f58bb9e1d66ac5fd3cc38ecaf6a080a3a4
judithmg <jmartigo33@gmail.com>
2021-05-10 11:44:57 +0200

    commented components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import Pending from './components/Pending';
-import SideNav from './components/SideNav';
-import Connections from './components/Connections';
+// import SideNav from './components/SideNav';
+// import Connections from './components/Connections';
 import About from './components/About';
 
 import './styles/App.scss';
-import './styles/Prueba.scss';
 
 function App() {
   return (
     <div className="App">
       <div className="left-column">
-        <SideNav />
+        {/* <SideNav /> */}
       </div>
       <div className="middle-column">
         <div className="middle-left" />
         <div className="middle-middle">
-          <Connections />
+          {/* <Connections /> */}
         </div>
         <div className="middle-right">
           <Pending />
