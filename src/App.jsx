8cbceb39ad6ad24a51f674a4583820dc5c07a437
judithmg <jmartigo33@gmail.com>
2021-05-12 08:40:55 +0200

    widths for sections on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import Inbox from './components/Inbox';
-// import Pending from './components/Pending';
-// import SideNav from './components/SideNav';
-// import Connections from './components/Connections';
-// import About from './components/About';
-// import Jobs from './components/JobsForYou';
-// import RecentSearch from './components/RecentSearch';
+import TopSkills from './components/TopSkills';
+import Pending from './components/Pending';
+import SideNav from './components/SideNav';
+import Connections from './components/Connections';
+import About from './components/About';
+import Jobs from './components/JobsForYou';
+import RecentSearch from './components/RecentSearch';
 
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="left-column">
-        {/* <SideNav /> */}
-      </div>
-      <div className="middle-column">
-        <div className="middle-left" />
-        <div className="middle-middle">
-          {/* <Connections /> */}
+      <div className="app-wrapper">
+        <div className="side-column">
+          <SideNav />
         </div>
-        <div className="middle-right">
-          {/* <Pending /> */}
-          {/* <RecentSearch /> */}
+        <div className="middle-column">
+          <div className="middle-upper">
+            <TopSkills />
+            <Connections />
+            <div className="middle-right">
+              <Pending />
+              <RecentSearch />
+            </div>
+          </div>
+          <About />
         </div>
-        {/* <About /> */}
+        <div className="side-column">
+          <Jobs />
+          <Inbox />
+        </div>
       </div>
-      <div className="right-column">
-        {/* <Jobs /> */}
-        <Inbox />
-      </div>
-
     </div>
   );
 }
